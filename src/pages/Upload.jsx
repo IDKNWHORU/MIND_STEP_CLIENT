1b95b86fc9f7a159e8d91b69c3411a60ef37fda1
IDKNWHORU <junho9341@gmail.com>
2023-08-28 13:13:17 +0900

    add upload box

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,9 +1,16 @@
+import { InboxOutlined } from '@ant-design/icons';
+import { message, Upload as AntdUpload } from 'antd';
+
+const { Dragger } = AntdUpload;
+
 export const Upload = _ => (
     <>
-        <article>
-            <header>상담 데이터 업로드</header>
-            <input type="file" />
-            <p>사용자님의 상담 데이터를 업로드해주세요!</p>
-        </article>
+        <Dragger>
+            <p className='ant-upload-drag-icon'>
+                <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">사용자님의 상담 데이터를 업로드해주세요!</p>
+            <p className="ant-upload-hint">단일 또는 대량 업로드를 지원합니다.</p>
+        </Dragger>
     </>
 )
