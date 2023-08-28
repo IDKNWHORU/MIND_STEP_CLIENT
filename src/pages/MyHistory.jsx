a43cafc3eede79e5531769eda91d9b3c8f49b4cc
IDKNWHORU <junho9341@gmail.com>
2023-08-28 16:35:33 +0900

    update timeline label

--- src/pages/MyHistory.jsx
+++ src/pages/MyHistory.jsx
@@ -10,26 +10,32 @@ export const MyHistory = _ => (
             mode="alternate"
             items={[
                 {
-                    children: '분석결과 내용1 2023-08-28 15:20',
+                    label: '2023-08-28 15:20',
+                    children: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                 },
                 {
-                    children: '분석결과 내용2 2023-08-29 15:20',
+                    label: '2023-08-29 15:20',
+                    children: '분석결과 내용2',
                     color: 'green',
                 },
                 {
+                    label: '2023-08-30 15:20',
                     dot: <ClockCircleOutlined style={{ fontSize: '16px' }} />,
-                    children: `분석결과 내용3 2023-08-30 15:20`,
+                    children: `분석결과 내용3`,
                 },
                 {
+                    label: '2023-09-01 15:20',
                     color: 'red',
-                    children: '분석결과 내용4 2023-09-01 15:20',
+                    children: '분석결과 내용4',
                 },
                 {
-                    children: '분석결과 내용5 2023-09-02 15:20',
+                    label: '2023-09-02 15:20',
+                    children: '분석결과 내용5',
                 },
                 {
+                    label: '2023-09-03 15:20',
                     dot: <ClockCircleOutlined style={{ fontSize: '16px' }} />,
-                    children: '분석결과 내용6 2023-09-03 15:20',
+                    children: '분석결과 내용6',
                 },
             ]}
         />
